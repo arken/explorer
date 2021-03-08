3d66bd00e8f53dda6aba5ac90e4490bd0ede37e8
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2021-03-07 23:37:14 -0700

    basic page layout

---
+++ src/components/nav/NavItems.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavItems = () => (
+  <ul>
+    <li>
+      <a href={"https://arken.io"} className={"toolbar-nav-items-link"}>
+        Home
+      </a>
+    </li>
+  </ul>
+);
+
+export default NavItems;
---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Base from "../components/Base";
+
+const NotFound = () => (
+  <Base pageName={"404"}>
+    <h1>404 Not Found!</h1>
+  </Base>
+);
+
+export default NotFound;
